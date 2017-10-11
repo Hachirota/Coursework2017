c2192311def084644ddd8c90e64cf8aa7fe1b4af
Carl <carloftheboyles@gmail.com>
2017-10-11 21:59:27 +0100

    11102017

--- ABC Music Notation.py
+++ ABC Music Notation.py
@@ -1,8 +1,15 @@
-music = open('hnr1.abc','r')
+input_file = open('hnr1.abc','r')
+
+music = input_file.read()
+
+output = str.split(music,'X:')
+#for line in music:
+ #   print(line)
 
 #for line in music:
-    #print(line)
+ #    if line[0:2] == "X:":
+  #       print(line)
 
-for line in music:
-     if line[0:2] == "X:":
-         print(line)
+print(type(output))
+
+print(output)
