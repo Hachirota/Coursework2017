63eb880b595be4be9b214887d40253365033a693
Hachirota <carloftheboyles@gmail.com>
2017-10-12 00:11:39 +0100

    12102017

--- ABC Music Notation.py
+++ ABC Music Notation.py
@@ -1,15 +1,25 @@
+def abc_formatter(song):
+    output_format = str.splitlines(song)
+    print(output_format[0]+" ",end='')
+    print("... " + output_format[1][2:],end='')
+    for line in output_format:
+        if line [0:2] == "M:":
+            print(" ... Time sig: " + line[2:] + " ",end='')
+        if line [0:2] == "K:":
+            print("... Key sig: " + line[2:])
+
 input_file = open('hnr1.abc','r')
 
 music = input_file.read()
-
 output = str.split(music,'X:')
-#for line in music:
- #   print(line)
 
-#for line in music:
- #    if line[0:2] == "X:":
-  #       print(line)
+output.pop(0)
 
-print(type(output))
+filelength = len(output)
 
-print(output)
+for item in output:
+    abc_formatter(item)
+
+print("-"*65)
+print("There are", (filelength), "tunes in the file.")
+print("-"*65)
